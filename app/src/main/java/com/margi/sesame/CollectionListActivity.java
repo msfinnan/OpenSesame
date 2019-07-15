2797c093922d745e5af8d444e4d8650c1d896f54
Margaret Finnan <msfinnan@gmail.com>
2019-07-15 15:52:03 -0700

    added logout functionality

--- app/src/main/java/com/margi/sesame/CollectionListActivity.java
+++ app/src/main/java/com/margi/sesame/CollectionListActivity.java
@@ -1,14 +1,73 @@
 package com.margi.sesame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
 
 public class CollectionListActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    private StorageReference storageReference;
+    // don't think I need this bc I don't have images, maybe once I add locations to the collection
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection_list);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_add :
+                //take users to AddCollectionActivity
+                //todo might need to change this once users can add locations to a collection
+                if (user != null && firebaseAuth != null) {
+                    startActivity(new Intent(CollectionListActivity.this,
+                            AddCollectionActivity.class));
+//                    finish(); //come back to this
+                }
+
+                break;
+            case R.id.action_sign_out :
+                if (user != null && firebaseAuth != null){
+                    firebaseAuth.signOut();
+
+                    startActivity(new Intent(CollectionListActivity.this,
+                            MainActivity.class));
+
+//                    finish(); //come back to this. 
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //get all Collections from Firestore
     }
 }
--- app/src/main/java/com/margi/sesame/LoginActivity.java
+++ app/src/main/java/com/margi/sesame/LoginActivity.java
@@ -1,29 +1,66 @@
 package com.margi.sesame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.net.PortUnreachableException;
+
+import javax.annotation.Nullable;
+
+import util.UserInfo;
 
 public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
     private Button createAcctButton;
 
+    private AutoCompleteTextView emailAddress;
+    private EditText password;
+
+    private ProgressBar progressBar;
+
+    //firebase instance variable
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users"); //"Users" is name of collection
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        progressBar = findViewById(R.id.login_progress);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         loginButton = findViewById(R.id.email_sign_in_button);
         createAcctButton = findViewById(R.id.create_account_button);
+        emailAddress = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
 
         createAcctButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +69,88 @@ public class LoginActivity extends AppCompatActivity {
                         CreateAccountActivity.class));
             }
         });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                loginEmailPasswordUser(emailAddress.getText().toString().trim(),
+                        password.getText().toString().trim());
+
+            }
+        });
+    }
+
+    private void loginEmailPasswordUser(String email, String password) {
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        if (!TextUtils.isEmpty(email)
+            && !TextUtils.isEmpty(password)) {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            //get info out of task
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            assert user != null;
+                            String currentUserId = user.getUid();
+
+                            //invoke collection reference to get a user from users collection
+
+                            collectionReference
+                                    .whereEqualTo("userId", currentUserId)
+                                    .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
+                                                            @Nullable FirebaseFirestoreException e) {
+                                            //query snapshot of all users with that id (just one user)
+
+                                            if (e != null) {
+                                                //if exception is not null, we have a problem
+                                                //probably can comment this out?
+                                            }
+                                            assert queryDocumentSnapshots != null;
+                                            if (!queryDocumentSnapshots.isEmpty()) {
+
+                                                progressBar.setVisibility(View.INVISIBLE);
+
+                                                // fetch info that we need about user that is logged in
+                                                //loop through to get user bc it is coming in as an arraylist or something like that
+
+                                                for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                                    UserInfo userInfo = UserInfo.getInstance();
+                                                    userInfo.setUsername(snapshot.getString("username")); //username is name of field in database
+                                                    userInfo.setUserId(snapshot.getString("userId"));
+
+                                                    //go to list activity //used global class instead of making an intent
+                                                    startActivity(new Intent(LoginActivity.this,
+                                                            AddCollectionActivity.class));
+//                                                    finish(); //will come back to this.
+                                                }
+
+
+                                            }
+
+                                        }
+                                    });
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressBar.setVisibility(View.INVISIBLE);
+
+                        }
+                    });
+        }else {
+            progressBar.setVisibility(View.INVISIBLE);
+
+            Toast.makeText(LoginActivity.this,
+                    "Please enter email and password",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 }
